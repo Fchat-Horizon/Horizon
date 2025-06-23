dfa8acf365caa994b24836c3119babf307f9a862
FatCat <82377182+FatCatClient@users.noreply.github.com>
2025-06-23 18:32:58 +0200

    vue3: Chat converted to composition API

--- chat/Chat.vue
+++ chat/Chat.vue
@@ -17,7 +17,7 @@
         {{ l('title') }}
         <a
           href="#"
-          @click.prevent="showLogs()"
+          @click.prevent="showLogs"
           class="btn"
           style="flex: 1; text-align: right"
         >
@@ -28,7 +28,11 @@
       <div class="card-body">
         <h4 class="card-title">{{ l('login.selectCharacter') }}</h4>
         <select v-model="selectedCharacter" class="form-control custom-select">
-          <option v-for="character in ownCharacters" :value="character">
+          <option
+            v-for="character in ownCharacters"
+            :value="character"
+            :key="character.id"
+          >
             {{ character.name }}
           </option>
         </select>
@@ -43,7 +47,7 @@
         </div>
       </div>
     </div>
-    <chat ref="chatview" v-else></chat>
+    <chatView ref="chatview" v-else></chatView>
     <modal
       :action="l('chat.disconnected.title')"
       :buttonText="l('action.cancel')"
@@ -66,26 +70,23 @@
 </template>
 
 <script lang="ts">
-  import log from 'electron-log'; //tslint:disable-line:match-default-export-name
-  import { Component, Hook, Prop } from '@f-list/vue-ts';
-  import Vue from 'vue';
+  import { defineComponent, ref, onMounted } from 'vue';
   import Modal from '../components/Modal.vue';
+  import ChatView from './ChatView.vue';
+  import Logs from './Logs.vue';
+  import log from 'electron-log';
   import { InlineDisplayMode, SimpleCharacter } from '../interfaces';
   import { Keys } from '../keys';
-  import ChatView from './ChatView.vue';
   import { errorToString, getKey } from './common';
   import core from './core';
   import l from './localize';
-  import Logs from './Logs.vue';
   import { init as profileApiInit } from './profile_api';
   import { AdManager } from './ads/ad-manager';
   import { EventBus } from './preview/event-bus';
 
-  type BBCodeNode = Node & { bbcodeTag?: string; bbcodeParam?: string };
-
   function copyNode(
     str: string,
-    node: BBCodeNode,
+    node: Node & { bbcodeTag?: string; bbcodeParam?: string },
     end: Node,
     range: Range,
     flags: { endFound?: true }
@@ -106,7 +107,7 @@
   }
 
   function scanNode(
-    node: BBCodeNode,
+    node: Node & { bbcodeTag?: string; bbcodeParam?: string },
     end: Node,
     range: Range,
     flags: { endFound?: true },
@@ -151,186 +152,199 @@
     return hide ? '' : str;
   }
 
-  @Component({
-    components: { chat: ChatView, modal: Modal, logs: Logs }
-  })
-  export default class Chat extends Vue {
-    @Prop({ required: true })
-    readonly ownCharacters!: SimpleCharacter[];
-    @Prop({ required: true })
-    readonly defaultCharacter!: number;
-    //tslint:disable-next-line:strict-boolean-expressions
-    selectedCharacter =
-      this.ownCharacters.find(x => x.id === this.defaultCharacter) ||
-      this.ownCharacters[0];
-    @Prop
-    readonly version?: string;
-    error = '';
-    connecting = false;
-    connected = false;
-    l = l;
-    copyPlain = false;
-
-    @Hook('mounted')
-    mounted(): void {
-      document.title = l('title', core.connection.character);
-      document.addEventListener('copy', ((e: ClipboardEvent) => {
-        if (this.copyPlain) {
-          this.copyPlain = false;
-          return;
-        }
-        const selection = document.getSelection();
-        if (selection === null || selection.isCollapsed) return;
-        const range = selection.getRangeAt(0);
-        let start = range.startContainer,
-          end = range.endContainer;
-        let startValue = '';
-        if (start instanceof HTMLElement) {
-          start = start.childNodes[range.startOffset];
-          if (<Node | undefined>start === undefined)
-            start = range.startContainer;
-          else
-            startValue =
-              start instanceof HTMLImageElement
-                ? start.alt
-                : scanNode(start.firstChild!, end, range, {});
-        } else
-          startValue = start.nodeValue!.substring(
-            range.startOffset,
-            start === range.endContainer ? range.endOffset : undefined
-          );
-        if (end instanceof HTMLElement && range.endOffset > 0)
-          end = end.childNodes[range.endOffset - 1];
-        e.clipboardData!.setData(
-          'text/plain',
-          copyNode(startValue, start, end, range, {})
-        );
-        e.preventDefault();
-      }) as EventListener);
-      window.addEventListener('keydown', e => {
-        if (
-          getKey(e) === Keys.KeyC &&
-          e.shiftKey &&
-          (e.ctrlKey || e.metaKey) &&
-          !e.altKey
-        ) {
-          this.copyPlain = true;
-          document.execCommand('copy');
-          e.preventDefault();
-        }
+  export default defineComponent({
+    name: 'Chat',
+    components: { chatView: ChatView, modal: Modal, logs: Logs },
+    props: {
+      ownCharacters: {
+        type: Array as () => SimpleCharacter[],
+        required: true
+      },
+      defaultCharacter: {
+        type: Number,
+        required: true
+      },
+      version: String
+    },
+    setup(props) {
+      log.debug('Chat component mounted', {
+        ownCharacters: props.ownCharacters,
+        defaultCharacter: props.defaultCharacter
       });
-      core.connection.onEvent('closed', isReconnect => {
-        log.debug('connection.closed', {
-          character: core.characters.ownCharacter?.name,
-          error: this.error,
-          isReconnect
-        });
-
-        if (isReconnect) (<Modal>this.$refs['reconnecting']).show(true);
-        if (this.connected) core.notifications.playSound('logout');
-        this.connected = false;
-        this.connecting = false;
-
-        AdManager.onConnectionClosed();
-        core.adCoordinator.clear();
-        EventBus.clear();
-
-        // tslint:disable-next-line:no-floating-promises
-        core.siteSession.onConnectionClosed();
-        core.cache.stop();
-
-        document.title = l('title');
-      });
-      core.connection.onEvent('connecting', async () => {
-        this.connecting = true;
-
-        log.debug('connection.connecting', {
-          character: core.characters.ownCharacter?.name
-        });
-
-        profileApiInit(
-          {
-            defaultCharacter: this.defaultCharacter,
-            animateEicons: core.state.settings.animatedEicons,
-            fuzzyDates: true,
-            inlineDisplayMode: InlineDisplayMode.DISPLAY_ALL
-          },
-          this.ownCharacters
-        );
-        if (core.state.settings.notifications)
-          await core.notifications.requestPermission();
-      });
-      core.connection.onEvent('connected', () => {
-        log.debug('connection.connected', {
-          character: core.characters.ownCharacter?.name
-        });
-
-        (<Modal>this.$refs['reconnecting']).hide();
-        this.error = '';
-        this.connecting = false;
-        this.connected = true;
-        core.notifications.playSound('login');
-        document.title = l('title.connected', core.connection.character);
-
-        // tslint:disable-next-line:no-floating-promises
-        core.siteSession.onConnectionEstablished();
-        core.cache.start((core.state as any).generalSettings, true);
-      });
-      core.watch(
-        () => core.conversations.hasNew,
-        hasNew => {
-          document.title =
-            (hasNew ? '💬 ' : '') +
-            l(
-              core.connection.isOpen ? 'title.connected' : 'title',
-              core.connection.character
-            );
-        }
+      const selectedCharacter = ref(
+        props.ownCharacters.find(x => x.id === props.defaultCharacter) ||
+          props.ownCharacters[0]
       );
-      core.connection.onError(e => {
-        log.debug('connection.error', {
-          error: errorToString(e),
-          character: core.characters.ownCharacter?.name
+      const error = ref('');
+      const connecting = ref(false);
+      const connected = ref(false);
+      const copyPlain = ref(false);
+      const chatview = ref<InstanceType<typeof ChatView>>();
+      const reconnecting = ref<InstanceType<typeof Modal>>();
+      const logsDialog = ref<InstanceType<typeof Logs>>();
+
+      function cancelReconnect() {
+        core.connection.close();
+        reconnecting.value?.hide();
+      }
+
+      function showLogs() {
+        logsDialog.value?.show();
+      }
+
+      async function connect() {
+        connecting.value = true;
+        core.notifications.initSounds([
+          'attention',
+          'login',
+          'logout',
+          'modalert',
+          'newnote',
+          'silence'
+        ]);
+        core.connection.connect(selectedCharacter.value.name);
+      }
+
+      function getChatView(): InstanceType<typeof ChatView> | undefined {
+        return chatview.value;
+      }
+
+      onMounted(() => {
+        document.title = l('title', core.connection.character);
+        document.addEventListener('copy', ((e: ClipboardEvent) => {
+          if (copyPlain.value) {
+            copyPlain.value = false;
+            return;
+          }
+          const selection = document.getSelection();
+          if (selection === null || selection.isCollapsed) return;
+          const range = selection.getRangeAt(0);
+          let start = range.startContainer,
+            end = range.endContainer;
+          let startValue = '';
+          if (start instanceof HTMLElement) {
+            start = start.childNodes[range.startOffset];
+            if (<Node | undefined>start === undefined)
+              start = range.startContainer;
+            else
+              startValue =
+                start instanceof HTMLImageElement
+                  ? start.alt
+                  : scanNode(start.firstChild!, end, range, {});
+          } else
+            startValue = start.nodeValue!.substring(
+              range.startOffset,
+              start === range.endContainer ? range.endOffset : undefined
+            );
+          if (end instanceof HTMLElement && range.endOffset > 0)
+            end = end.childNodes[range.endOffset - 1];
+          e.clipboardData!.setData(
+            'text/plain',
+            copyNode(startValue, start, end, range, {})
+          );
+          e.preventDefault();
+        }) as EventListener);
+        window.addEventListener('keydown', e => {
+          if (
+            getKey(e) === Keys.KeyC &&
+            e.shiftKey &&
+            (e.ctrlKey || e.metaKey) &&
+            !e.altKey
+          ) {
+            copyPlain.value = true;
+            document.execCommand('copy');
+            e.preventDefault();
+          }
         });
-
-        if ((<Error & { request?: object }>e).request !== undefined) {
-          //catch axios network errors
-          this.error = l('login.connectError', errorToString(e));
-          this.connecting = false;
-        } else throw e;
+        core.connection.onEvent('closed', isReconnect => {
+          log.debug('connection.closed', {
+            character: core.characters.ownCharacter?.name,
+            error: error.value,
+            isReconnect
+          });
+          if (isReconnect) reconnecting.value?.show(true);
+          if (connected.value) core.notifications.playSound('logout');
+          connected.value = false;
+          connecting.value = false;
+          AdManager.onConnectionClosed();
+          core.adCoordinator.clear();
+          EventBus.clear();
+          core.siteSession.onConnectionClosed();
+          core.cache.stop();
+          document.title = l('title');
+        });
+        core.connection.onEvent('connecting', async () => {
+          connecting.value = true;
+          log.debug('connection.connecting', {
+            character: core.characters.ownCharacter?.name
+          });
+          profileApiInit(
+            {
+              defaultCharacter: props.defaultCharacter,
+              animateEicons: core.state.settings.animatedEicons,
+              fuzzyDates: true,
+              inlineDisplayMode: InlineDisplayMode.DISPLAY_ALL
+            },
+            props.ownCharacters
+          );
+          if (core.state.settings.notifications)
+            await core.notifications.requestPermission();
+        });
+        core.connection.onEvent('connected', () => {
+          log.debug('connection.connected', {
+            character: core.characters.ownCharacter?.name
+          });
+          reconnecting.value?.hide();
+          error.value = '';
+          connecting.value = false;
+          connected.value = true;
+          core.notifications.playSound('login');
+          document.title = l('title.connected', core.connection.character);
+          core.siteSession.onConnectionEstablished();
+          core.cache.start((core.state as any).generalSettings, true);
+        });
+        core.watch(
+          () => core.conversations.hasNew,
+          hasNew => {
+            document.title =
+              (hasNew ? '💬 ' : '') +
+              l(
+                core.connection.isOpen ? 'title.connected' : 'title',
+                core.connection.character
+              );
+          }
+        );
+        core.connection.onError(e => {
+          log.debug('connection.error', {
+            error: errorToString(e),
+            character: core.characters.ownCharacter?.name
+          });
+          if ((<Error & { request?: object }>e).request !== undefined) {
+            error.value = l('login.connectError', errorToString(e));
+            connecting.value = false;
+          } else throw e;
+        });
       });
+
+      return {
+        l,
+        selectedCharacter,
+        error,
+        connecting,
+        connected,
+        copyPlain,
+        chatview,
+        reconnecting,
+        logsDialog,
+        cancelReconnect,
+        showLogs,
+        connect,
+        getChatView,
+        ownCharacters: props.ownCharacters,
+        version: props.version
+      };
     }
-
-    cancelReconnect(): void {
-      core.connection.close();
-      (<Modal>this.$refs['reconnecting']).hide();
-    }
-
-    showLogs(): void {
-      (<Logs>this.$refs['logsDialog']).show();
-    }
-
-    async connect(): Promise<void> {
-      this.connecting = true;
-
-      // skipping await
-      // tslint:disable-next-line: no-floating-promises
-      core.notifications.initSounds([
-        'attention',
-        'login',
-        'logout',
-        'modalert',
-        'newnote',
-        'silence'
-      ]);
-
-      core.connection.connect(this.selectedCharacter.name);
-    }
-
-    getChatView(): ChatView | undefined {
-      return this.$refs['chatview'] as ChatView;
-    }
-  }
+  });
 </script>
 
 <style lang="scss">
