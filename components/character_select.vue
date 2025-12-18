ea1b4b67683f68d49c6a6cb7ae741e2ccdcb5419
AriannaAltomare <arianna.altomare1994@gmail.com>
2025-12-18 12:45:50 -0800

    Convert character_select.vue to Composition API

--- components/character_select.vue
+++ components/character_select.vue
@@ -1,29 +1,35 @@
 <template>
-  <select :value="value" @change="emit" class="form-select">
-    <option v-for="character in characters" :value="character.id">
+  <select :value="selectValue" @change="onSelectChange" class="form-select">
+    <option
+      v-for="character in characters"
+      :key="character.id"
+      :value="character.id"
+    >
       {{ character.name }}
     </option>
     <slot></slot>
   </select>
 </template>
 
-<script lang="ts">
-  import { Component, Prop } from '@f-list/vue-ts';
-  import Vue from 'vue';
+<script setup lang="ts">
+  import { computed } from 'vue';
   import { SimpleCharacter } from '../interfaces';
   import * as Utils from '../site/utils';
 
-  @Component
-  export default class CharacterSelect extends Vue {
-    @Prop({ required: true })
-    readonly value!: number;
+  const props = defineProps<{
+    selectValue?: number;
+  }>();
 
-    get characters(): SimpleCharacter[] {
-      return Utils.characters;
-    }
+  const emit = defineEmits<{
+    (e: 'update:selectValue', value: number): void;
+  }>();
 
-    emit(evt: Event): void {
-      this.$emit('input', parseInt((<HTMLSelectElement>evt.target).value, 10));
-    }
-  }
+  const characters = computed<SimpleCharacter[]>(() => Utils.characters);
+
+  const onSelectChange = (evt: Event): void => {
+    const target = evt.target as HTMLSelectElement;
+    const newValue = parseInt(target.value, 10);
+
+    emit('update:selectValue', newValue);
+  };
 </script>
